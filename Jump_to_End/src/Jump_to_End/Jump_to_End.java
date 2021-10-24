7395bb9ef128b8d51c967630eb5ee4930af3d3b0
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-24 19:01:32 -0400

    Update Jump_to_End.java

--- Jump_to_End/src/Jump_to_End/Jump_to_End.java
+++ Jump_to_End/src/Jump_to_End/Jump_to_End.java
@@ -1,4 +1,22 @@
 package Jump_to_End;
 
 public class Jump_to_End {
+  
+  public boolean Jump_to_End(int[] userArray){
+  
+    if(userArray.length == 0){
+    return true;
+    }
+    
+    if(userArray.length == null){
+    return true;
+    }
+    
+    while(first_position<=end_position){
+      end_position = Math.max(end_position, first_position+userArray[first_position++]);
+        if(end_position>=userArray.length - 1){
+        return true;}
+    }
+  return false;
+  }
 }
