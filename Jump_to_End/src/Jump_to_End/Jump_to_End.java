51a39e08dd7c0a5fa4d966e92a4088c9b4e08352
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-27 09:58:15 -0400

    Update Jump_to_End.java

--- Jump_to_End/src/Jump_to_End/Jump_to_End.java
+++ Jump_to_End/src/Jump_to_End/Jump_to_End.java
@@ -3,23 +3,24 @@ package Jump_to_End;
 /*This class is the implementation class. */
 
 public class Jump_to_End {
-  
-  public boolean Jump_to_End(int[] userArray){
-  
-    if(userArray.length == 0){ //If the user didn't put any array, then there is only one way to get to the destination from the starting point.
-    return true;
+
+    public boolean Jump_to_End(int[] userArray){
+
+        if(userArray.length == 0){ //If the user didn't put any array, then there is only one way to get to the destination from the starting point.
+            return true;
+        }
+
+        if(userArray == null){ //If there is no input to the array, then there is only one way to get to the destination from the starting point.
+            return true;
+        }
+
+        int lastArrayPosition = userArray.length - 1;
+        for(int i = userArray.length - 2; i>=0; i--){
+            if(i + userArray[i]>=lastArrayPosition){
+                lastArrayPosition = i;
+            }
+            return true;
+        }
+        return false;
     }
-    
-    if(userArray.length == null){ //If there is no input to the array, then there is only one way to get to the destination from the starting point.
-    return true;
-    }
-    
-    int lastArrayPosition = userArray.length - 1;
-    for(int i = userArray.length - 2; i>=0; i--){
-      if(i + userArray[i]>=lastArrayPosition){
-        lastArrayPosition = i;
-      }
-    }
-  return false;
-  }
 }
