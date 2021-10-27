75f3a21c6845bc2401904b0c5bb946e462e7b150
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-27 09:58:40 -0400

    Update Main.java

--- Jump_to_End/src/Jump_to_End/Main.java
+++ Jump_to_End/src/Jump_to_End/Main.java
@@ -2,6 +2,10 @@ package Jump_to_End;
 
 public class Main {
     public static void main(String[] args){
-        
+        Jump_to_End  je = new Jump_to_End();
+        int[] userArray= {2,3,1,1,4};
+        boolean result;
+        result = je.Jump_to_End(userArray);
+        System.out.println(result);
     }
 }
