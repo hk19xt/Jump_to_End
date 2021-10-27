e4910907405fbdb4a816b86c0700b9ad114f7ac5
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-26 21:40:08 -0400

    Update Jump_to_End.java

--- Jump_to_End/src/Jump_to_End/Jump_to_End.java
+++ Jump_to_End/src/Jump_to_End/Jump_to_End.java
@@ -1,18 +1,20 @@
 package Jump_to_End;
 
+/*This class is the implementation class. */
+
 public class Jump_to_End {
   
   public boolean Jump_to_End(int[] userArray){
   
-    if(userArray.length == 0){
+    if(userArray.length == 0){ //If the user didn't put any array, then there is only one way to get to the destination from the starting point.
     return true;
     }
     
-    if(userArray.length == null){
+    if(userArray.length == null){ //If there is no input to the array, then there is only one way to get to the destination from the starting point.
     return true;
     }
     
-    while(first_position<=end_position){
+    while(first_position<=end_position){ //If 
       end_position = Math.max(end_position, first_position+userArray[first_position++]);
         if(end_position>=userArray.length - 1){
         return true;}
