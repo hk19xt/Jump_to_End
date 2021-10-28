7512db04ccf56ac62d7916eefebc5b50e06eab18
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-28 13:45:00 -0400

    Update Main.java

--- Jump_to_End/src/Jump_to_End/Main.java
+++ Jump_to_End/src/Jump_to_End/Main.java
@@ -1,9 +1,14 @@
 package Jump_to_End;
 
+/*This is the main class. The users can change the values in the input array. 
+The calculation whether the first element can jump to the end position will be conducted by the implementation class. 
+Name: Hyejin Kim
+Date: 10/29/2021 */
+
 public class Main {
     public static void main(String[] args){
         Jump_to_End  je = new Jump_to_End();
-        int[] userArray= {2,3,1,1,4};
+        int[] userArray= {2,3,1,1,4}; //The users can change the array values here.
         boolean result;
         result = je.Jump_to_End(userArray);
         System.out.println(result);
